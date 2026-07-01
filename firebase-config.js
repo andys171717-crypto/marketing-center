621d11fadfe578cc45a52e0bf4bdb785a56ea48b
Andy <andys171717@gmail.com>
2026-07-02 06:39:20 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// Firebase Config - Marketing Center
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_Nrh-1VsGLlbyIMmt8POjpc1w7RB1U6s",
+    authDomain: "marketing-center-94f6f.firebaseapp.com",
+    projectId: "marketing-center-94f6f",
+    storageBucket: "marketing-center-94f6f.firebasestorage.app",
+    messagingSenderId: "202926336637",
+    appId: "1:202926336637:web:84fcec7fccaf1a1fc95f14"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
