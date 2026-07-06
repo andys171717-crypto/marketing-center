f29e60d7acfc660a143c91a7748a14a75e57f8eb
Andy <andys171717@gmail.com>
2026-07-06 11:52:08 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,15 @@
 // Firebase Config - Marketing Center
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
 import {
     getFirestore
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+import {
+    getAuth,
+    GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA_Nrh-1VsGLlbyIMmt8POjpc1w7RB1U6s",
@@ -17,4 +23,18 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { app, db };
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+export {
+
+    app,
+
+    db,
+
+    auth,
+
+    googleProvider
+
+};
